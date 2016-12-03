ce5753a035a0c42f79012e0319653ff7e20da36f
Colin Frink <frinkco@my.ccsu.edu>
2016-12-03 23:29:51 +0000

    User validation, passwod hashing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161119022451) do
+ActiveRecord::Schema.define(version: 20161203232418) do
 
   create_table "games", force: :cascade do |t|
     t.string   "name"
@@ -30,11 +30,11 @@ ActiveRecord::Schema.define(version: 20161119022451) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.string   "view"
-    t.string   "genre"
-    t.string   "publisher"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "email"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
